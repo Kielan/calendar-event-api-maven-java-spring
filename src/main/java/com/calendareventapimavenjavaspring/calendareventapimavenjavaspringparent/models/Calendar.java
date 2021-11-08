891f52d772e43988c79f58ffe51c92cb059f3a8a
kielan <ki.lemons@gmail.com>
2021-11-07 18:21:05 -0800

    fix vim indentation 4

--- src/main/java/com/calendareventapimavenjavaspring/calendareventapimavenjavaspringparent/models/Calendar.java
+++ src/main/java/com/calendareventapimavenjavaspring/calendareventapimavenjavaspringparent/models/Calendar.java
@@ -13,44 +13,44 @@ import javax.persistence.OneToOne;
 
 @Entity
 public class Calendar {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  private String name;
-  private String type;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String type;
 //  @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 //  private User user;
 //  @OneToMany(cascade = CascadeType.ALL, orphanRemoval=true, fetch = FetchType.LAZY)
 //  private List<CalendarEvent> events;
 
-  public Calendar() {
-    super();
-  }
+	public Calendar() {
+		super();
+	}
 
-  public Calendar(Long id, String name/*, User user*/) {
-    super();
-    this.id = id;
-    this.name = name;
+	public Calendar(Long id, String name/*, User user*/) {
+		super();
+		this.id = id;
+		this.name = name;
 //    this.user = user;
-  }
+	}
 
-  public String getType() {
-    return type;
-  }
+	public String getType() {
+		return type;
+	}
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- src/main/java/com/calendareventapimavenjavaspring/calendareventapimavenjavaspringparent/models/CalendarEvent.java
+++ src/main/java/com/calendareventapimavenjavaspring/calendareventapimavenjavaspringparent/models/CalendarEvent.java
@@ -15,21 +15,21 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class CalendarEvent {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  private String title;
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-  private LocalDateTime eventDateTime;
-  private Integer duration;
-  private String location;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime eventDateTime;
+	private Integer duration;
+	private String location;
 //  @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 //  private LocalDateTime reminderDateTime;
 //  private boolean reminderSent;
 
 	public CalendarEvent(){}
 
-  public CalendarEvent(Long id, String title, LocalDateTime eventDateTime, Integer duration, String location) {
+	public CalendarEvent(Long id, String title, LocalDateTime eventDateTime, Integer duration, String location) {
 		super();
 		this.id = id;
 		this.title = title;
@@ -38,22 +38,22 @@ public class CalendarEvent {
 		this.location = location;
 	}
 
-  public String getTitle() {
-    return title;
-  }
+	public String getTitle() {
+		return title;
+ 	}
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+	public void setTitle(String title) {
+  	this.title = title;
+	}
 
-  public LocalDateTime getEventDateTime() {
-    return eventDateTime;
-  }
+	public LocalDateTime getEventDateTime() {
+		return eventDateTime;
+	}
 
-  public void setEventDateTime(LocalDateTime eventDateTime) {
-    this.eventDateTime = eventDateTime;
-  }
-	
+	public void setEventDateTime(LocalDateTime eventDateTime) {
+		this.eventDateTime = eventDateTime;
+	}
+
 	public Integer getDuration() {
 		return duration;
 	}
@@ -62,13 +62,13 @@ public class CalendarEvent {
 		this.duration = duration;
 	}
 
-  public String getLocation() {
-    return location;
+	public String getLocation() {
+		return location;
   }
 
   public void setLocation(String location) {
     this.location = location;
-  }
+	}
 
 //  public LocalDateTime getReminderDateTime() {
 //    return reminderDateTime;
